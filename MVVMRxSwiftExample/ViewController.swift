5d8f548c1ac143b0e691b98907b4197da0f1393a
Simran <simran5590@gmail.com>
2020-09-03 01:21:50 -0700

    Initial Commit

---
+++ MVVMRxSwiftExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMRxSwiftExample
+//
+//  Created by jc on 2020-09-03.
+//  Copyright © 2020 jc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
